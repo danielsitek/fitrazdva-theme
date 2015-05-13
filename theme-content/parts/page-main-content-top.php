8bfdf9bd1dc6329d2a372afd83b6b78f986bed8a
Daniel Sitek <dan.sitek@edgedesign.cz>
2015-05-14 00:04:53 +0200

    upraven zapis scriptu pro vypisovani custom menu do sablony

--- theme-content/parts/page-main-content-top.php
+++ theme-content/parts/page-main-content-top.php
@@ -1,24 +1,13 @@
 <?php
 use FitRazDva\SetPageSubmenu;
-$fitrazdva_set_page_submenu = new SetPageSubmenu;
-$fitrazdva_set_page_submenu->onPageInit( $post->ID );
+$fitrazdva_set_page_submenu = new SetPageSubmenu( $post->ID );
 
 $page_main_content_cLasses = ['page-main-content'];
-$fitrazdva_set_page_submenuName;
-
 
 if ($fitrazdva_set_page_submenu->hasMenu) {
     array_push($page_main_content_cLasses, 'has-submenu');
-    $fitrazdva_set_page_submenuName = $fitrazdva_set_page_submenu->getMenuName();
 }
 
 echo '<div class="' . implode(' ', $page_main_content_cLasses) . '">';
 
-if ( isset($fitrazdva_set_page_submenuName) ) {
-    echo $fitrazdva_set_page_submenuName;
-}
-// if ($fitrazdva_set_page_submenu->hasMenu) {
-    // echo '<nav class="nav-submenu">';
-    $fitrazdva_set_page_submenu->getMenu();
-    // echo '</nav>';
-// }
+$fitrazdva_set_page_submenu->getMenu();
