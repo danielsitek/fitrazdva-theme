9611ead40ad10554923f3fd0f7c285bb2535cff0
Daniel Sitek <dan.sitek@edgedesign.cz>
2015-05-15 01:11:33 +0200

    fixnuty sablony

--- theme-content/single.php
+++ theme-content/single.php
@@ -23,13 +23,6 @@
 
 			<?php the_content(); ?>
 			</div>
-			<footer>
-				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-				<p><?php the_tags(); ?></p>
-			</footer>
-			<?php do_action( 'foundationpress_post_before_comments' ); ?>
-			<?php comments_template(); ?>
-			<?php do_action( 'foundationpress_post_after_comments' ); ?>
 		</article>
 	<?php endwhile;?>
 
