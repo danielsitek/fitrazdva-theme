f389b753abfb8d8aa2a4d4685de5fbeb87a086c5
Daniel Sitek <dan.sitek@edgedesign.cz>
2015-06-23 14:51:39 +0200

    added function for extending tinyMCE

--- theme-content/functions.php
+++ theme-content/functions.php
@@ -46,6 +46,9 @@ require_once('library/custom-header.php');
 // Customized gallery
 require_once('library/custom-gallery.php');
 
+// Extending TinyMCE editor
+require_once('library/tinymce-init.php');
+
 /**
  * Init for WP Admin section
  */
