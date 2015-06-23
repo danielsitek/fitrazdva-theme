bbd0e6beb309d36fcd3f3f521d6d43172b7bd08a
Daniel Sitek <dan.sitek@edgedesign.cz>
2015-06-23 15:18:13 +0200

    added google tag manager

--- theme-content/functions.php
+++ theme-content/functions.php
@@ -49,6 +49,9 @@ require_once('library/custom-gallery.php');
 // Extending TinyMCE editor
 require_once('library/tinymce-init.php');
 
+// Insert Google Tag Manager
+require_once('library/google-tag-managager-controller.php');
+
 /**
  * Init for WP Admin section
  */
