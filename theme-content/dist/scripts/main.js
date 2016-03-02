46f15e05815cd541a40561451b3ca418c155027f
Daniel Sitek <dan.sitek@edgedesign.cz>
2016-03-02 09:44:44 +0100

    rebuild assets

--- theme-content/dist/scripts/main.js
+++ theme-content/dist/scripts/main.js
@@ -3,7 +3,7 @@
  * This is WordPress theme for website FitRazDva.cz. It`s build on top of slightly modified FoundationPress starter theme, based on Foundation 5 framework by Zurb.
  * 
  * @version:       v0.3.0
- * @updated:       07-01-2016
+ * @updated:       01-03-2016
  * @license:       MIT
  */
  /*!
