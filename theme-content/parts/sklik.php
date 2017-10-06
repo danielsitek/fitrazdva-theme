125b22c4cab13f50af1e8fae0016042770edb56b
Daniel Sitek <dan.sitek@gmail.com>
2017-10-06 21:20:19 +0200

    fix sklik kodu

--- theme-content/parts/sklik.php
+++ theme-content/parts/sklik.php
@@ -7,4 +7,4 @@
 
 ?>
 <!-- Měřicí kód Sklik.cz -->
-<iframe width="119" height="22" frameborder="0" scrolling="no" data-src="//c.imedia.cz/checkConversion?c=100018567&color=ffffff&v=" src="/skin/default/img/empty.gif" style="visibility: hidden;"></iframe>
+<iframe width="119" height="22" frameborder="0" scrolling="no" src="//c.imedia.cz/checkConversion?c=100018567&color=ffffff&v=" style="visibility: hidden;"></iframe>
